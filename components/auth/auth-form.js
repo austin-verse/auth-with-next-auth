7c8ba9c26c4d9751c3673a60ac0fbbe05d5eaaf6
Gyeomyi Hwang <austin.hwang.dev@gmail.com>
2023-06-13 17:49:44 +0900

    Feat: auth페이지 리다이렉션 서버&클라이언트 사이드 코드 추가

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,6 +1,7 @@
 import { useRef, useState } from "react";
 import classes from "./auth-form.module.css";
 import { signIn } from "next-auth/client";
+import { useRouter } from "next/router";
 // 사용자 생성 함수
 async function CreateUser(email, password) {
 	const response = await fetch("/api/auth/signup", {
@@ -18,8 +19,10 @@ async function CreateUser(email, password) {
 }
 
 function AuthForm() {
+	const router = useRouter();
 	const emailInputRef = useRef();
 	const passwordInputRef = useRef();
+	// 로그인인지 회원가입인지
 	const [isLogin, setIsLogin] = useState(true);
 
 	function switchAuthModeHandler() {
@@ -33,14 +36,23 @@ function AuthForm() {
 		const enteredPassword = passwordInputRef.current.value;
 		// optional: Validation
 		if (isLogin) {
+			// signIn은 프로미스 항상 resolve, 에러가 발생하면 내용 result.error가 null이 아닌 무언가 발생
 			const result = await signIn("credentials", {
 				redirect: false,
 				email: enteredEmail,
 				password: enteredPassword,
 			});
 			console.log(result);
+			// 에러가 발생하지 않았을 시 (error = null)
+			if (!result.error) {
+				// replace는 현재 URL를 다른 URL로 바꿔줌
+				// 새로고침 없음
+				router.replace("/profile");
+			}
+
 			// 로그인 하기
 		} else {
+			// 회원가입
 			try {
 				const result = await CreateUser(enteredEmail, enteredPassword);
 				console.log(result);
--- pages/auth.js
+++ pages/auth.js
@@ -1,7 +1,34 @@
-import AuthForm from '../components/auth/auth-form';
+import { getSession } from "next-auth/client";
+import AuthForm from "../components/auth/auth-form";
+import { useEffect, useState } from "react";
 
 function AuthPage() {
-  return <AuthForm />;
+	const [isLoading, setIsLoading] = useState(true);
+	useEffect(() => {
+		getSession().then((session) => {
+			if (session) {
+				window.location.href = "/profile";
+			} else {
+				setIsLoading(false);
+			}
+		});
+	}, []);
+	if (isLoading) {
+		return <p>loading</p>;
+	}
+	return <AuthForm />;
 }
 
 export default AuthPage;
+
+export async function getServerSideProps(context) {
+	const session = await getSession({ req: context.req });
+	if (session) {
+		return {
+			redirect: {
+				destination: "/profile",
+				permanent: false,
+			},
+		};
+	}
+}
